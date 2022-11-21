3f95c6fe6c2c6d2cc43c023c4427350559e56238
Gregor <gregorseidelgiongo@hotmail.com>
2022-11-21 16:50:32 -0300

    commit

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,5 +1,6 @@
 import { StyledRegisterVideo } from "./styles";
 import React from "react";
+import { createClient } from '@supabase/supabase-js';
 
 function useForm() {
     const [values, setValues] = React.useState("");
@@ -20,6 +21,14 @@ function useForm() {
     };
 }
 
+const PROJECT_URL ='https://tloyzvoifrngskhlyoqd.supabase.co'
+const PUBLIC_KEY =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsb3l6dm9pZnJuZ3NraGx5b3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjkwNTQwMDMsImV4cCI6MTk4NDYzMDAwM30.l30aM_8a5YpYstsWi95EQ0On-M3LuiXRwGjhk7JoDvk'
+const supabase = createClient(PROJECT_URL, PUBLIC_KEY)
+
+function getThumbmail(url){
+    return `https://img.youtube.com/vi/${url.split("v=")[1]}/hqdefault.jpg`
+}
+
 export default function RegisterVideo() {
     const [formViseble, setFormViseble] = React.useState(false)
     let formCadastro = useForm()
@@ -33,6 +42,16 @@ export default function RegisterVideo() {
                 <form onSubmit={(evento) => {
                     evento.preventDefault();
                     console.log(formCadastro.values);
+
+                    supabase.from("video").insert({
+                        title: formCadastro.values.titulo,
+                        url: formCadastro.values.url,
+                        thumb: getThumbmail(formCadastro.values.url),
+                        playlist: formCadastro.values.playlist
+                    })
+                    .then((e) =>{console.log(e)})
+                    .catch((e) =>{console.log(e)})
+
                     setFormViseble(false);
                     formCadastro.clearForm();
                 }}>
@@ -46,6 +65,11 @@ export default function RegisterVideo() {
                         <input
                             placeholder="URL"
                             name="url"
+                            onChange={formCadastro.handleChange}
+                        />
+                        <input
+                            placeholder="Playlist"
+                            name="playlist"
                             onChange={formCadastro.handleChange}
                         />
                         <button type="submit">Cadastrar</button>
